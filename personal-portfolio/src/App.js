265987b930c754cf8c4532f6f4eee6afe4407932
Fitore <ttahirifitore@gmail.com>
2023-07-21 17:31:10 +0200

    navbar done

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import { Navigation } from './components/NavBar';
+import './App.css';
+
+function App()
+{
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
