cbdc109e65d0330041dca24a0140b9a3826cfcdd
Fitore <ttahirifitore@gmail.com>
2023-07-21 18:31:30 +0200

    skills and projects done

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import { Navigation } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import './App.css';
 
 function App()
 {
   return (
     <div className="App">
-      <Navigation/>
+      <Navigation />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
